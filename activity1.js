a38141d22843ab2dd5f46143132540d63ba9a4e9
hihiMedha <79608709+hihiMedha@users.noreply.github.com>
2021-08-02 15:07:03 -0700

    Add files via upload

---
+++ activity1.js
@@ -0,0 +1,13 @@
+ score=0;
+ function updatescore(){
+     score=score+1;
+     document.getElementById("score").innerHTML="score: "+score;
+ }
+
+ function savescore(){
+     localStorage.setItem("score", score); 
+ }
+
+ function nextpage(){
+     window.location="";
+ }
